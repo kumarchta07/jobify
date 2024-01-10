509e281b1e142b6e3580dea13cae21cb48e72ec1
Arvind <kumarchta07@gmail.com>
2024-01-10 15:59:17 -0600

    update user

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,16 +1,18 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // set the directory where uploaded files will be stored
-    cb(null, "public/uploads");
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname;
-    // set the name of the uploaded file
-    cb(null, fileName);
-  },
-});
+import DataURIParser from "datauri/parser.js";
+
+import path from "path";
+
+const storage = multer.memoryStorage();
+
 const upload = multer({ storage });
+
+const parser = new DataURIParser();
+
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
